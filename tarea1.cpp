5018868551e7a605ab010dd4ef1ddd7dbc55a2c6
Your <you@gmail.com>
2021-03-03 15:09:03 +0000

    primer ejemplo

---
+++ tarea1.cpp
@@ -0,0 +1,18 @@
+//numeros primos desde 1 hasta 100
+
+//incluir librerias
+
+#include <iostream>
+#include <cmath>
+
+//using namespace std; mala costumbre
+
+int main(void)
+{
+  int m = 1;
+  int t = 100;
+  std::cout << "m: "<< m << "\n";
+  std::cout << "t: "<< t << "\n";
+  return 0;
+}
+
